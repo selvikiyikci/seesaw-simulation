25fffe5ded9a1259efb70fa4dd74046ff43b26e8
selvikiyikci <selvikiyikci@gmail.com>
2025-10-27 10:32:42 +0300

    added a 2d context with retina scaling

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-(function(){
-  console.log("Seesaw project initialized");
+(function () {
+  const canvas = document.getElementById("cv");
+  const context = canvas.getContext("2d");
+  const pixelRatio = window.devicePixelRatio || 1;
+  canvas.width = canvas.clientWidth * pixelRatio;
+  canvas.height = canvas.clientHeight * pixelRatio;
+
+  context.scale(pixelRatio, pixelRatio);
+
+  // Test amaçlı arka plan rengi eklendi
+  context.fillStyle = "#333";
+  context.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
 })();
